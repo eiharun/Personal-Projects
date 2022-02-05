f0d0c111a84c7e45811be069d267ec16c01a4569
eiharun <92404926+eiharun@users.noreply.github.com>
2022-02-05 12:19:33 -0500

    Updates
    BadHash

---
+++ MCC/BadHash/BadHashCTF.py
@@ -0,0 +1,30 @@
+state = []
+hash_len=50
+for i in range(0,hash_len):
+	state.append(0)
+def round():
+	for i in range(0,hash_len):
+		state[i]=(state[i]+state[(i+1)%hash_len])%128
+def absorb(message):
+	message_list = list(message)
+	message_list_ints = []
+	for m in message_list:
+		message_list_ints.append(ord(m))
+	message_len = len(message_list_ints)
+	for i in range(hash_len):
+		state[i]=state[i]+message_list_ints[i%message_len]
+def hash(message):
+	absorb(message)
+	for i in range(0,21):
+		round()
+	hash_string = ""
+	for c in range(0,hash_len):
+		add_str = hex(state[c])[2:]
+		if len(add_str) < 2:
+			add_str="0"+add_str
+		hash_string+=add_str
+	#print('0x' + hash_string)
+	return "0x"+hash_string
+#message = input("message>")
+
+#hash(message)
---
+++ MCC/BadHash/BadHashSolution_BruteForce.py
@@ -0,0 +1,51 @@
+import BadHashCTF
+
+#Used from https://gist.github.com/pazdera/1121315
+import string
+ALLOWED_CHARACTERS = string.printable
+NUMBER_OF_CHARACTERS = len(ALLOWED_CHARACTERS)
+
+def characterToIndex(char):
+    return ALLOWED_CHARACTERS.index(char)
+
+def indexToCharacter(index):
+    if NUMBER_OF_CHARACTERS <= index:
+        raise ValueError("Index out of range.")
+    else:
+        return ALLOWED_CHARACTERS[index]
+
+def next(string):
+    """ Get next sequence of characters.
+    Treats characters as numbers (0-255). Function tries to increment
+    character at the first position. If it fails, new character is
+    added to the back of the list.
+    It's basically a number with base = 256.
+    :param string: A list of characters (can be empty).
+    :type string: list
+    :return: Next list of characters in the sequence
+    :rettype: list
+    """
+    if len(string) <= 0:
+        string.append(indexToCharacter(0))
+    else:
+        string[0] = indexToCharacter((characterToIndex(string[0]) + 1) % NUMBER_OF_CHARACTERS)
+        if characterToIndex(string[0]) is 0:
+            return list(string[0]) + next(string[1:])
+    return string
+
+def main(hash):
+    sequence = list()
+    while True:
+        sequence = next(sequence)
+        #print(sequence)
+        message = "masoncc{" + msg.join(sequence) + "}"
+        if BadHashCTF.hash(message) == hash:
+            break
+        print(message)
+
+hash = input('Input hash: ')
+msg_hash = ""
+message_list = []
+msg = ''
+#msg_hash = BadHashCTF.hash(msg.join(main()))
+main(hash)
